e560a210906bb469d734e89de824a2183b76d7ea
JJ Zhang <115352148+L1MOJ@users.noreply.github.com>
2025-02-03 20:18:42 -0500

    feat: 检出代码

--- ai-code-reviewer-sdk/src/main/java/duke/limoj/sdk/AiCodeReviewer.java
+++ ai-code-reviewer-sdk/src/main/java/duke/limoj/sdk/AiCodeReviewer.java
@@ -1,5 +1,10 @@
 package duke.limoj.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * AiCodeReviewer
  *
@@ -9,7 +14,26 @@ package duke.limoj.sdk;
 
 public class AiCodeReviewer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Test1111");
+
+        // 1. 代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        processBuilder.directory(new File("."));
+
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            diffCode.append(line);
+        }
+
+        int exitCode = process.waitFor();
+        System.out.println("Exit with code:" + exitCode);
+
+        System.out.println("Code Review: " + diffCode.toString());
     }
 }
