e13af73cc713681fd53d03179d338d741f1e1f11
JJ Zhang <115352148+L1MOJ@users.noreply.github.com>
2025-02-12 12:38:00 -0500

    feat: integrate sonarqube v4

--- ai-code-reviewer-test/src/test/java/duke/limoj/test/ApiTest.java
+++ ai-code-reviewer-test/src/test/java/duke/limoj/test/ApiTest.java
@@ -21,7 +21,7 @@ public class ApiTest {
     public void test() {
         System.out.println("aaaa12311213213");
         System.out.println("aaaa212313");
-        System.out.println("aaaa123133");
+        System.out.println("aaaa1asd23133");
 
     }
 }
